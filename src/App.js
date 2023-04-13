a5bd556e7e3a9f81ee91c9a9f0270eeecda1272e
preetiyadav30 <py132430@gmail.com>
2023-04-13 11:36:04 +0530

    modify app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          my first website of react is hosted using gitgub
         </a>
       </header>
     </div>
